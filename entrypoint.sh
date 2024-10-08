84a4a8c863477796eca67034d5b5b94555734280
David Bernal <leo.bernal1946@gmail.com>
2024-10-08 17:22:21 +0200

    chore: remove cuotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,7 +44,7 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 exec java -classpath "/opt/lib/checkstyle.jar:java-check-style-validations/target/java-check-style-validations-0.0.1-SNAPSHOT.jar" \
   com.puppycrawl.tools.checkstyle.Main "${INPUT_WORKDIR}" \
-  -c java-check-style-rest/src/main/resources/checkstyle-style.xml "${OPTIONAL_PROPERTIES_FILE}" "${OPTIONAL_EXCLUDED_PATHS}" -f xml \
+  -c ${INPUT_CHECKSTYLE_CONFIG} "${OPTIONAL_PROPERTIES_FILE}" "${OPTIONAL_EXCLUDED_PATHS}" -f xml \
   | reviewdog -f=checkstyle \
       -name="checkstyle" \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
