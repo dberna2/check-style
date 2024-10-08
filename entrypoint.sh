eb1cf2a4e8f9e8cbf6e17720afaa51435c89240b
David Bernal <leo.bernal1946@gmail.com>
2024-10-08 17:28:52 +0200

    chore: print var values

--- entrypoint.sh
+++ entrypoint.sh
@@ -48,8 +48,8 @@ echo "INPUT_WORKDIR ${INPUT_WORKDIR}"
 echo "OPTIONAL_PROPERTIES_FILE ${OPTIONAL_PROPERTIES_FILE}"
 
 exec java -classpath "/opt/lib/checkstyle.jar:java-check-style-validations/target/java-check-style-validations-0.0.1-SNAPSHOT.jar" \
-  com.puppycrawl.tools.checkstyle.Main \"${INPUT_WORKDIR}" \
-  -c "${INPUT_CHECKSTYLE_CONFIG}" "${OPTIONAL_PROPERTIES_FILE}" "${OPTIONAL_EXCLUDED_PATHS}" -f xml \
+  com.puppycrawl.tools.checkstyle.Main \ "${INPUT_WORKDIR}" \
+  -c style-rest/src/main/resources/checkstyle-style.xml "${OPTIONAL_PROPERTIES_FILE}" "${OPTIONAL_EXCLUDED_PATHS}" -f xml \
   | reviewdog -f=checkstyle \
       -name="checkstyle" \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
