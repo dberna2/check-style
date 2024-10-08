b57e25799b7c912166b615ea44ebda45776419e0
David Bernal <leo.bernal1946@gmail.com>
2024-10-08 21:18:03 +0200

    chore: remove echo steps

--- entrypoint.sh
+++ entrypoint.sh
@@ -42,26 +42,6 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 # run check
 { echo "Run check with"; java -jar /opt/lib/checkstyle.jar --version; } | sed ':a;N;s/\n/ /;ba'
 
-echo "CHECK_STYLE_CLASS_PATH ${CHECK_STYLE_CLASS_PATH}"
-echo "INPUT_CHECKSTYLE_CONFIG ${INPUT_CHECKSTYLE_CONFIG}"
-echo "INPUT_WORKDIR ${INPUT_WORKDIR}"
-echo "OPTIONAL_PROPERTIES_FILE ${OPTIONAL_PROPERTIES_FILE}"
-
-echo "before exec"
-
-echo "exec java -classpath \"${CHECK_STYLE_CLASS_PATH}\" \
-  com.puppycrawl.tools.checkstyle.Main \"${INPUT_WORKDIR}\" \
-  -c \"${INPUT_CHECKSTYLE_CONFIG}\" \"${OPTIONAL_PROPERTIES_FILE}\" \"${OPTIONAL_EXCLUDED_PATHS}\" -f xml \
-  | reviewdog -f=checkstyle \
-        -name=\"checkstyle\" \
-        -reporter=\"${INPUT_REPORTER:-github-pr-check}\" \
-        -filter-mode=\"${INPUT_FILTER_MODE:-added}\" \
-        -fail-on-error=\"${INPUT_FAIL_ON_ERROR:-false}\" \
-        -level=\"${INPUT_LEVEL}\" \
-        \"${INPUT_REVIEWDOG_FLAGS}\" -"
-
-echo "after exec"
-
 exec java -classpath "${CHECK_STYLE_CLASS_PATH}" \
   com.puppycrawl.tools.checkstyle.Main "${INPUT_WORKDIR}" \
   -c "${INPUT_CHECKSTYLE_CONFIG}" "${OPTIONAL_PROPERTIES_FILE}" "${OPTIONAL_EXCLUDED_PATHS}" -f xml \
